7512a44abaa57ca34b5cbc08699d16f6cd056100
John Wynkoop <jlwynkoop@gmail.com>
2017-11-15 23:55:51 -0500

    initial commit

---
+++ ess.tf
@@ -0,0 +1,74 @@
+#Copyright 2017 Reactive Ops Inc.
+#
+#Licensed under the Apache License, Version 2.0 (the “License”);
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an “AS IS” BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+data "alicloud_images" "green_image" {
+  most_recent = true
+  name_regex = "${var.ess_green_image_name_regex}"
+}
+
+data "alicloud_images" "blue_image" {
+  most_recent = true
+  name_regex = "${var.ess_blue_image_regex}"
+}
+
+resource "alicloud_ess_scaling_group" "esg" {
+  count              = "${var.az_count}"
+  vswitch_id         = "${element(var.ess_vswitch_ids, count.index)}" 
+  min_size           = "${var.ess_scaling_min_size}"
+  max_size           = "${var.ess_scaling_max_size}"
+  scaling_group_name = "${var.ess_scaling_group_name}-${element(var.ess_vswitch_ids, count.index)}"
+  removal_policies   = "${var.ess_removal_policies}"
+  loadbalancer_ids   = [ "${var.ess_loadbalancer_ids}" ]
+}
+
+resource "alicloud_ess_scaling_configuration" "esg-greenconfig" {
+  count                       = "${var.az_count}"
+  scaling_configuration_name  = "${var.ess_scaling_config_name}-green"          
+  scaling_group_id            = "${element(alicloud_ess_scaling_group.esg.*.id, count.index)}"
+  active                      = "${var.ess_deployment == "blue" ? false : true}"
+  enable                      = true
+  image_id                    = "${data.alicloud_images.green_image.images.0.id}"
+  instance_type               = "${var.ess_instance_type}"
+  security_group_id           = "${var.ess_sg_id}"
+#  force_delete                = true
+}
+
+resource "alicloud_ess_scaling_configuration" "esg-blueconfig" {
+  count                       = "${var.az_count}"
+  scaling_configuration_name  = "${var.ess_scaling_config_name}-blue"
+  scaling_group_id            = "${element(alicloud_ess_scaling_group.esg.*.id, count.index)}"
+  active                      = "${var.ess_deployment == "blue" ? true : false}"
+  enable                      = true
+  image_id                    = "${data.alicloud_images.blue_image.images.0.id}"
+  instance_type               = "${var.ess_instance_type}"
+  security_group_id           = "${var.ess_sg_id}"
+}
+
+resource "alicloud_ess_scaling_rule" "scaleup" {
+  count             = "${var.az_count}"
+  scaling_rule_name = "ScaleUp"
+  scaling_group_id  = "${element(alicloud_ess_scaling_group.esg.*.id, count.index)}"
+  adjustment_type   = "QuantityChangeInCapacity"
+  adjustment_value  = "${var.ess_scaleup_size}"
+  cooldown          = "${var.ess_scalup_cooldown}"
+}
+
+resource "alicloud_ess_scaling_rule" "scaledown" {
+  count             = "${var.az_count}"
+  scaling_rule_name = "ScaleDown"
+  scaling_group_id  = "${element(alicloud_ess_scaling_group.esg.*.id, count.index)}"
+  adjustment_type   = "QuantityChangeInCapacity"
+  adjustment_value  = "${var.ess_scaledown_size}"
+  cooldown          = "${var.ess_scaledown_cooldown}"
+}
---
+++ variables.tf
@@ -0,0 +1,114 @@
+#Copyright 2017 Reactive Ops Inc.
+#
+#Licensed under the Apache License, Version 2.0 (the “License”);
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an “AS IS” BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+## Variables for accessing Aliyun Cloud ##
+variable "alicloud_access_key" {}
+variable "alicloud_secret_key" {}
+variable "alicloud_region" {
+  description = "region to deploy to, default is China East 1 (cn-hangzhou)"
+  default = "cn-hangzhou"
+}
+
+variable "alicloud_azs" {
+  description = "comma separated string of availability zones in order of precedence, defaults are all zones for cn-hangzhou"
+  default = "cn-hangzhou-b, cn-hangzhou-c, cn-hangzhou-d"
+
+}
+
+variable "az_count" {
+  description = "number of active availability zones in the VPC"
+  default = "3"
+}
+
+
+## Variables for ESS Groups, Configurations, and Rules ##
+variable "ess_scaling_min_size" {
+  description = "minimum size for ess groups"
+  default = 1
+}
+
+variable "ess_scaling_max_size" {
+  description = "maximum size for ess groups"
+  default = 3
+}
+
+variable "ess_sg_id" {
+  description = "id of the security group to use for ESS instances"
+  default = ""
+}
+
+variable "ess_removal_policies" {
+  description = "removal policy to use for generated ess groups"
+  default = ["OldestInstance", "NewestInstance"]
+}
+
+variable "ess_instance_type" {
+  description = "instance type to use for ess scaling groups"
+  default = "ecs.n4.large"
+}
+
+variable "ess_green_image_name_regex" {
+  description = "regex to match against for image name used in ess"
+  default     = "^centos_6\\w{1,5}[64].*"
+}
+
+variable "ess_loadbalancer_ids" {
+  description = "ids of the SLB to assign the ESS to"
+  default     = []
+}
+
+variable "ess_scalup_cooldown" {
+  description = "cooldown period for scaleup rule"
+  default     = 300
+}
+variable "ess_scaledown_cooldown" {
+  description = "cooldown period for scaledown rule"
+  default     = 300
+}
+
+variable "ess_scaling_group_name" {
+  description = "name of the scaling group"
+  default     = "tf-esg"
+}
+
+variable "ess_scaling_config_name" {
+  description = "name of the scaling config to create"
+  default     = "tf-scalingconfig"
+}
+
+variable "ess_scaleup_size" {
+  description = "size of scaleup actions"
+  default     = 1
+}
+
+variable "ess_scaledown_size" {
+  description = "size of scaledown actions"
+  default     = -1
+}
+
+variable "ess_vswitch_ids" {
+  type = "list"
+  description = "vswitches to create ess groups in"
+}
+
+variable "ess_deployment" {
+  description = "blue/green deployment to make active"
+  default = "green"
+}
+
+variable "ess_blue_image_regex" {
+  description = "regex to match for images in the blue deployment"
+  default = "^centos_6\\w{1,5}[64].*"
+}
+
